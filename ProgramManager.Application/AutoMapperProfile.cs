687ae5423cde9633225d945020171b6181aae3eb
Michael Ihim <mikiplid@yahoo.com>
2023-10-08 22:48:38 +0100

    changes to bring in auto mapper

---
+++ ProgramManager.Application/AutoMapperProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using ProgramManager.Domain.Entities;
+using ProgramManager.Application.Dtos;
+using ProgramManager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ProgramManager.Application
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+      
+            CreateMap<Domain.Entities.Application, ApplicationDto>();
+            CreateMap<PersonalInformationQuestion, PersonalInformationQuestionDto>();
+            CreateMap<ProfileQuestion, ProfileQuestionDto>();
+            CreateMap<Program, ProgramDto>();
+            CreateMap<ProgramType, ProgramTypeDto>().ReverseMap();
+            CreateMap<Qualification, QualificationDto>().ReverseMap();
+            CreateMap<Question, QuestionDto>();
+            CreateMap<Skill, SkillDto>().ReverseMap();
+            CreateMap<Stage, StageDto>();
+            CreateMap<VideoInterviewAdditionalField, VideoInterviewAdditionalFieldDto>();
+        }
+    }
+}
